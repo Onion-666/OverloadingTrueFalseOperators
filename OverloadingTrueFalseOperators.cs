0b398cdfd4a51902953670a74d9965edd3a3e06a
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-11-10 19:27:30 +0300

    Добавьте файлы проекта.

---
+++ OverloadingTrueFalseOperators.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class MyClass
+{
+    public int code;
+    public MyClass(int n)
+    {
+        code = n;
+    }
+    public static bool operator true(MyClass obj)
+    {
+        if (obj.code >= 5)
+            return true;
+        else
+            return false;
+    }
+    public static bool operator false(MyClass obj)
+    {
+        if (obj)
+            return false;
+        else
+            return true;
+    }
+}
+class Programm
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass(10);
+        while(obj)
+        {
+            Console.Write(obj.code + " ");
+            obj.code--;
+        }
+        Console.WriteLine();
+    }
+}
